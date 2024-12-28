f7bf67523283931758df8bcbaf614b446932d497
Taher <taher.marengoze@gmail.com>
2024-12-28 14:08:59 +0200

    OOP Assignment 1

---
+++ C43-G03-OOP01/Program.cs
@@ -0,0 +1,343 @@
+﻿using static System.Console;
+using MyNamespace;
+using MyNamespace2;
+
+namespace C43_G03_OOP01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Print.WeekDays();
+            DrawLine();
+
+            //2
+            Print.SeasonMonthRange();
+            DrawLine();
+
+            //3
+            _ = new PermissionManagment();
+            DrawLine();
+
+            //4
+            Print.PrimaryColorCheck();
+            DrawLine();
+
+            //Close
+            Hold();
+        }
+
+        static void DrawLine()
+        {
+            WriteLine("\n".PadRight(60,'=') + "\n");
+        }
+
+        static void Hold()
+        {
+            WriteLine("Press ENTER to exit");
+            ReadLine();
+        }
+
+
+    }
+
+    #region Part #1
+
+    //Please, check the MyClassLibrary project
+
+    //accessing internal members from outside the assembly
+
+    class Part1Demo
+    {
+        // Uncommenting any of the below will give this compile error: inaccessible due to its protection level
+
+        //MyClass myClass = new();
+        //MyStruct myStruct = new();
+        //IMyInterface myInterface;
+        //MyEnum enumValue = MyEnum.A;
+
+        // public objects can be accessed normally without any error
+        MyPublicClass myPublicClass = new();
+        MyPublicStruct myPublicStruct = new();
+        IMyPublicInterface myPublicInterface;
+        MyPublicEnum myPublicEnum = MyPublicEnum.X;
+    }
+
+    public class ClassA : DemoClass
+    {
+        public ClassA()
+        {
+            //_private = 1; //private
+            //_privateProtected = 2; //depite inheritance private will make it inaccessible from outside the assembly
+            _protected = 3; //accessible due to being protected despite being outside the assembly
+            //_internal = 4; //internal will not be accessible outside the assembly
+            _protectedInternal = 5; //same as protected
+            _public = 6; //public accessible anywhere
+        }
+    }
+
+    public class ClassB
+    {
+        DemoClass demo = new();
+
+        public ClassB()
+        {
+            demo._public = 6; //only public will be accessible
+            //other level of access will not be accessible because this class is not inheriting
+            //and its outside the assembly of the DemoClass
+        }
+    }
+
+    enum Rating
+    {
+        OneStar = 1,
+        TwoStars,
+        ThreeStars,
+        FourStars,
+        FiveStars
+    }
+
+    //combined enum values
+    [Flags]
+    enum Articles
+    {
+        Programing = 1,
+        History = 2,
+        Innovation = 4,
+        Health = 8,
+        Economics = 16
+    }
+
+    public class EnumMethods
+    {
+        public EnumMethods()
+        {
+            _ = Enum.TryParse(typeof(Rating), "ThreeStars", false, out object rating);
+            Console.WriteLine(rating); //Prints: ThreeStars
+
+            _ = Enum.TryParse(typeof(Rating), "6", false, out rating);
+            Console.WriteLine(rating); //Prints: 6
+
+
+            //=======================================================
+
+            Articles subscribedArticles =
+                Articles.Programing |
+                Articles.History |
+                Articles.Innovation |
+                Articles.Health |
+                Articles.Economics;
+
+            Console.WriteLine(subscribedArticles);
+
+            //Leave only programing
+            subscribedArticles &= Articles.Programing;
+            Console.WriteLine(subscribedArticles);
+
+            subscribedArticles ^= Articles.Innovation;
+            Console.WriteLine(subscribedArticles);
+
+            //=======================================================
+
+            Console.WriteLine(Enum.GetNames(typeof(Rating)));
+
+            //=======================================================
+
+            Console.WriteLine(Enum.IsDefined(typeof(Rating), 6));
+
+        }
+    }
+    #endregion
+
+    #region Part #2
+
+    #region 1. Weekdays Enum
+    //1. Create an enum called "WeekDays" with the days of the week (Monday to Sunday) as its members.
+    //   Then, write a C# program that prints out all the days of the week using this enum.
+
+    enum WeekDays
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+    #endregion
+
+    #region 2. Seasons Months Range
+
+    //2. Create an enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as  its members.
+    //   Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season.
+    //   Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+
+    enum Seas
+    {
+        Spring = 1, Summer, Autumn, Winter
+    }
+
+    #endregion
+
+    #region 3. Permissions
+    //3. Assign the following Permissions (Read, write, Delete, Execute) in a form of Enum.
+    //   • Create Variable from previous Enum to Add and Remove Permission from variable, check if specific Permission is existed inside variable
+
+    [Flags]
+    enum Permission
+    {
+        Read = 1,
+        Write = 2,
+        Delete = 4,
+        Execute = 8
+    }
+
+    class PermissionManagment
+    {
+        Permission permission;
+
+        public PermissionManagment()
+        {
+            //Add all permissions
+            permission = Permission.Read | Permission.Write | Permission.Delete | Permission.Execute;
+            WriteLine(permission);
+
+            //Remove all except Read
+            permission &= Permission.Read;
+            WriteLine(permission);
+
+            //Add Write if not exist or remove if exists (Toggle)
+            permission ^= Permission.Write;
+            WriteLine(permission);
+
+            permission ^= Permission.Write;
+            WriteLine(permission);
+
+            if (permission.HasFlag(Permission.Read))
+            {
+                WriteLine("Has " + Permission.Read);
+            }
+            else
+            {
+                WriteLine("Don't have " + Permission.Read);
+            }
+        }
+
+
+    }
+
+    #endregion
+
+    #region 4. Primary Colors
+    //4. Create an enum called "Colors" with the basic colors (Red, Green, Blue) as its members.
+    //   Write a C# program that takes a color name as input from the user and
+    //   displays a message indicating whether the input color is a primary color or not.
+
+    enum Colors
+    {
+        Red = 1,
+        Green = 2,
+        Blue = 3,
+    }
+
+
+    #endregion
+
+    class Print
+    {
+        public static void WeekDays()
+        {
+            WriteLine(JoinList(Enum.GetNames(typeof(WeekDays))));
+        }
+
+        public static void SeasonMonthRange()
+        {
+            object result;
+            bool isValid;
+
+            do
+            {
+                Write("Enter Season Name: ");
+
+                isValid = Enum.TryParse(typeof(Seas), ReadLine(), true, out result!);
+
+                if (isValid)
+                    isValid = Enum.IsDefined(typeof(Seas), result);
+
+                if (!isValid)
+                    InvalidInput();
+            }
+            while (!isValid);
+
+            Seas inputSeason = (Seas)result!;
+
+            switch (inputSeason)
+            {
+                case Seas.Spring:
+                WriteLine($"{inputSeason} ranges from March to May");
+                break;
+
+                case Seas.Summer:
+                WriteLine($"{inputSeason} ranges from June to August");
+                break;
+
+                case Seas.Autumn:
+                WriteLine($"{inputSeason} ranges from September to November");
+                break;
+
+                case Seas.Winter:
+                WriteLine($"{inputSeason} ranges from December to February");
+                break;
+
+                default:
+                WriteLine("There are 4 seasons in a year, perhaps there is more in another alternate reality");
+                break;
+            }
+        }
+
+        public static void PrimaryColorCheck()
+        {
+            object result;
+            bool isValid;
+
+            do
+            {
+                Write("Enter Color: ");
+
+                isValid = Enum.TryParse(typeof(Colors), ReadLine(), true, out result!);
+
+                if (isValid)
+                    isValid = Enum.IsDefined(typeof(Colors), result);
+
+                if (!isValid)
+                    InvalidInput();
+            }
+            while (!isValid);
+
+            WriteLine($"Is Primary Color: {isValid}");
+        }
+
+        private static string JoinList(string[] list)
+        {
+            string joinedList = "";
+
+            foreach (string item in list)
+            {
+                joinedList += $"{item},";
+            }
+
+            return joinedList.Remove(joinedList.Length - 1);
+        }
+
+        static void InvalidInput()
+        {
+            ForegroundColor = ConsoleColor.DarkRed;
+            WriteLine("Invalid Input! ... Try Again");
+            ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+
+    #endregion
+}
---
+++ MyClassLibrary/CodeFile.cs
@@ -0,0 +1,173 @@
+﻿namespace MyClassLibrary
+{
+    using MyNamespace;
+
+    public class Demo
+    {
+        //accessing internal members from inside the assembly
+
+        MyClass myClass = new();
+
+        MyStruct myStruct = new ();
+
+        IMyInterface myInterface;
+
+        MyEnum enumValue = MyEnum.A;
+    }
+}
+
+namespace MyNamespace
+{
+    //a namespace may contain the following
+    class MyClass
+    {
+
+    }
+
+    struct MyStruct
+    {
+
+    }
+
+    interface IMyInterface
+    {
+
+    }
+
+    enum MyEnum
+    {
+        A, B, C
+    }
+    //and are declared internal by default
+
+    //but can be declare as public
+    public class MyPublicClass
+    {
+
+    }
+
+    public struct MyPublicStruct
+    {
+
+    }
+
+    public interface IMyPublicInterface
+    {
+
+    }
+
+    public enum MyPublicEnum
+    {
+        X, Y, Z
+    }
+}
+
+namespace MyNamespace2
+{
+    public class DemoClass
+    {
+        public DemoClass()
+        {
+            Start();
+        }
+        //a class may include the following
+
+        //attribute / member variable / field
+        int x;
+
+        // function / method
+        void Start()
+        {
+            _private = 50;
+
+            Console.WriteLine(_private);
+        }
+
+        //property (full / auto / indexer)
+        int y { get; set; }
+
+        // event
+
+        // and are declared as private by default
+
+        //access modifiers
+        private int _private;
+        private protected int _privateProtected;
+        protected int _protected;
+        internal int _internal;
+        protected internal int _protectedInternal;
+        public int _public;
+    }
+
+    public class ClassA : DemoClass
+    {
+        public ClassA()
+        {
+            // can't access becuase its private
+            //_private = 1;
+
+            //accessible from outside the class but only to ClassB because
+            //it inherit from DemoClass
+            _privateProtected = 2;
+
+            //accessible because ClassB inherit from DemoClass
+            _protected = 3;
+
+            //accessible because its internal
+            _internal = 4;
+
+            //accessible because its internal + inheritance
+            _protectedInternal = 5;
+
+            //accessible because its public
+            _public = 6;
+        }
+    }
+
+    class ClassB
+    {
+        ClassB()
+        {
+            DemoClass demo = new();
+            // can't access becuase its private
+            //demo._private = 1;
+
+            //compile error: inaccessible due to its protection level
+            //can't access because ClassC don't inherit from DemoClass
+            //demo._privateProtected = 2;
+
+            //compile error: inaccessible due to its protection level
+            // ClassC don't inherit from DemoClass
+            //demo._protected = 3;
+
+            //accessible despite not being inheriting from DemoClass due to being internal
+            demo._internal = 4;
+
+            //accessible because its protected internal
+            demo._protectedInternal = 5;
+
+            //accessible because its public
+            demo._public = 6;
+        }
+    }
+
+    //interfaces
+    interface IAssignment
+    {
+        //may contain
+
+        //signature of a method
+        void Solve();
+
+        //signature of a property
+        int Difficulty { get; set; }
+
+        //Default Implemented Method
+        void Submit()
+        {
+            Console.WriteLine("Commit => Push => Email");
+        }
+    }
+
+    //interfaces may have internal or public access modifiers
+}
