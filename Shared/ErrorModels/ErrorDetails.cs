2eb6a9eb1cef1fe58641574b9279217736b3a4e5
Taher <taher.marengoze@gmail.com>
2025-04-24 13:33:25 +0200

    API Session 4 Assignment Part 1

---
+++ Shared/ErrorModels/ErrorDetails.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.ErrorModels;
+
+public class ErrorDetails
+{
+    public int StatusCode { get; set; }
+
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    public string ErrorMessage { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+}
---
+++ Store.Api/Middlewares/GlobalErrorHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using Shared.ErrorModels;
+
+namespace Store.Api.Middlewares;
+
+public class GlobalErrorHandlingMiddleware(
+    RequestDelegate next,
+    ILogger<GlobalErrorHandlingMiddleware> logger)
+{
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Oops, bad thing happened: {ex}");
+
+            await HandleException(httpContext, ex);
+        }
+    }
+
+    private async Task HandleException(HttpContext httpContext, Exception ex)
+    {
+        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        httpContext.Response.ContentType = "application/json";
+
+        var response = new ErrorDetails
+        {
+            StatusCode = 100,
+            ErrorMessage = ex.Message,
+        };
+
+        //TODO:
+
+        response.StatusCode = httpContext.Response.StatusCode;
+
+        await httpContext.Response.WriteAsync($"{response}");
+    }
+}
