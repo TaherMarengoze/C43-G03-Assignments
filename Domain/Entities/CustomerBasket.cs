52e5c86983057bfa89991f5ac7cb3d2f48fbb524
Taher <taher.marengoze@gmail.com>
2025-04-25 13:02:22 +0300

    API Session 4 Assignment Part 8

--- Domain/Entities/CustomerBasket.cs
+++ Domain/Entities/CustomerBasket.cs
@@ -4,5 +4,5 @@ public class CustomerBasket
 {
     public string Id { get; set; }
 
-    public IEnumerable<BasketItem> BasketItems { get; set; }
+    public IEnumerable<BasketItem> Items { get; set; }
 }
---
+++ Presentation/Controllers/BasketController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstraction;
+using Shared.Dto.Basket;
+
+namespace Presentation.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class BasketController(IServiceManager serviceManager) : ControllerBase
+{
+    [HttpGet("{id}")]
+    public async Task<ActionResult<BasketDto>> Get(string id)
+    {
+        var basket = await serviceManager.BasketService.GetBasketAsync(id);
+
+        return Ok(basket);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<BasketDto>> Update(BasketDto basketDto)
+    {
+        var updatedBasketDto =
+            await serviceManager.BasketService.UpdateBasketAsync(basketDto);
+
+        return Ok(updatedBasketDto);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> Delete(string id)
+    {
+        _ = await serviceManager.BasketService.DeleteBasketAsync(id);
+        return NoContent();
+    }
+}
