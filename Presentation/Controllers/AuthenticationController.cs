37a65e579b13b4937d5e50cbdf1e80d11b929ca1
Taher <taher.marengoze@gmail.com>
2025-04-25 21:06:14 +0300

    API Session 5 Assignment Part 5

---
+++ Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Abstraction;
+using Shared.Dto.Identity;
+
+namespace Presentation.Controllers;
+
+public class AuthenticationController(IServiceManager serviceManager)
+    : ApiController
+{
+    [HttpPost("Register")]
+    public async Task<ActionResult<UserResultDto>> Register(RegisterDto registerDto)
+        => Ok(await serviceManager.AuthenticationService.RegisterAsync(registerDto));
+
+    [HttpPost("Login")]
+    public async Task<ActionResult<UserResultDto>> Login(LoginDto loginDto)
+        => Ok(await serviceManager.AuthenticationService.LoginAsync(loginDto));
+}
